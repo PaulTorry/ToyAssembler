d3b4c2d3bc4fb891700462f24dc5b40954ca6e92
Paul T <PaulTorry@users.noreply.github.com>
2023-01-14 17:22:53 +0000

    updates toview and machine

--- machine.js
+++ machine.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable no-multiple-empty-lines, no-unused-vars */
 
 
 
@@ -68,32 +68,35 @@ class simpleComputer {
     this.running = false
     this.acc = 0
     this.ops = [
-      (x) => { this.acc = x }, //                          in 0
-      (x) => { this.mem[x] = this.acc }, //                to 1
-      (x) => { this.acc = this.mem[x] }, //                fr 2
-      (x) => { this.acc = this.acc + this.mem[x] }, //     add 3
-      (x) => { this.acc = this.acc - this.mem[x] }, //     sub 4
-      (x) => { if (!this.acc) this.currentOp = x }, //      to 5
-      (x) => { this.print(this.acc) }, //                   fr 6
-      (x) => { console.log('Error command unknown') }, //   res 7
-      (x) => { this.running = false; this.print('End code:' + x) } //                  fr 8
+      (x) => { this.acc = x }, //                                      in  0
+      (x) => { this.mem[x] = this.acc }, //                            to  1
+      (x) => { this.acc = this.mem[x] }, //                            fr  2
+      (x) => { this.acc = this.acc + this.mem[x] }, //                 add 3
+      (x) => { this.acc = this.acc - this.mem[x] }, //                 sub 4
+      (x) => { if (!this.acc) this.currentOp = x }, //                 to  5
+      (x) => { this.print(x || this.acc) }, //                         fr  6
+      (x) => { console.log('mError command unknown') }, //              res 7
+      (x) => { this.running = false; this.print('mEnd code:' + x) } //  end 8
     ]
   }
 
-  run (instructions = [[6, 2], [8, 0]], debug = console.log) {
+  run (instructions = [[6, 2], [8, 0]], debug = () => {}) {
     this.instructionMem = instructions
     this.running = true
     while (this.running) {
+      //  console.log('running', this.currentOp, this.running)
       const [op, on] = this.instructionMem[this.currentOp]
+      debug('  mDebug:line:', this.currentOp, 'op:', op, 'on:', on)
       this.ops[op](on)
-      debug('i', this.currentOp, 'op', op, 'on', on, this.acc, this.mem[0])
+      debug('    mDebug:line:', this.currentOp, 'acc', this.acc, 'mem', this.mem)
       this.currentOp++
     }
+    console.log(this.printed)
     return this.printed
   }
 
   print (x) {
-    console.log(x)
+    console.log(' mPprinting', x)
     this.printed = this.printed.concat([[this.currentOp, x]])
   }
 }
