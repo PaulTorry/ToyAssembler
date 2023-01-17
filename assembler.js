461aa281f171ba8880bfebd84cbf29ce8bc65d7b
Paul T <PaulTorry@users.noreply.github.com>
2023-01-17 22:37:32 +0000

    define

--- assembler.js
+++ assembler.js
@@ -14,3 +14,36 @@ function parseAssembler (text) { // eslint-disable-line no-unused-vars
 
   return replaced
 }
+
+function advancedAssembler (text) { // eslint-disable-line no-unused-vars
+  const variables = ['cat']
+
+  const lines = text.split('\n')
+
+  const trimmed = lines.map((x) => x.trim())
+
+  const separated = trimmed.map((x) => x.split(' '))
+  console.log(...separated)
+
+  separated.forEach((e, i) => { if (e.length > 2) console.log('to many words', e, i) })
+
+  const replaced = []
+  separated.forEach((v, i, a) => {
+    const [com, val] = v
+    if (assCode.indexOf(com) + 1) {
+      const value = variables.indexOf(val) + 1 ? variables.indexOf(val) : Number(val)
+      replaced.push([assCode.indexOf(com), value])
+    } else if (com === 'def') {
+      variables.push(val)
+    } else if (variables.indexOf(com) + 1) {
+      replaced.concat([0, Number(val)], [1, variables.indexOf(com)])
+    } else if (com === 'mult') {
+      // muliply memory by accumulator
+      // let multCode = [['def', 'stack'], ['def', 'store'], ['to', 'stack'], ]
+    }
+    //  {return [assCode.indexOf(com), Number(val)]}
+  })
+  console.log('hi', ...replaced, variables)
+
+  return replaced
+}
